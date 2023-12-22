a37abecc058c759a0228a614fc3d42bc26eb1ad0
rahul7276 <146751303+rahul7276@users.noreply.github.com>
2023-12-22 11:23:27 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -10,6 +10,7 @@
 def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
+     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
      docker build -t ${ecr_repoName} .
      docker tag ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
     """
